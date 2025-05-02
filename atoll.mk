c60c1424e336cb0fc28aec61c8df7b209a3969aa
rickastlee <45075899+rickastlee@users.noreply.github.com>
2025-05-02 07:21:38 +0200

    sm6250-common: Don't build AudioFX

--- atoll.mk
+++ atoll.mk
@@ -56,6 +56,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
